c262fcafca11f59cb88485757bdb9739107dfbc6
sabre230 <44850786+sabre230@users.noreply.github.com>
2023-04-13 13:23:11 -0700

    Prototyping slash command handlers

--- Program.cs
+++ Program.cs
@@ -1,18 +1,22 @@
 ﻿using Discord;
+using Discord.Interactions;
 using Discord.Net;
 using Discord.WebSocket;
 using System.Reflection;
 using Discord.Commands;
 using Discord.Utils;
-
 using System.Xml.Linq;
+using System;
+using Newtonsoft.Json;
+using System.Windows.Input;
 
-
-namespace PostBot;
+//namespace PostBot;
 
 public  class Program
 {
     private DiscordSocketClient _client;
+    private CommandService _commandService;
+    private CommandHandler _commandHandler;
 
     public ulong Guild;
     public ulong TextChannel;
@@ -23,11 +27,24 @@ public  class Program
     public async Task MainAsync()
 
     {
+        // Ensure our config files are in place
+        Console.WriteLine("Checking for config.xml and announcement.xml...");
+        CheckForFiles();
+        Console.WriteLine("Done!");
+
         // Get our settings before doing anything else!
         await GetConfigFromXML();
 
+        // Create a message cache for reasons
         var _config = new DiscordSocketConfig { MessageCacheSize = 100 };
+
+        // Create a new client instance (just one)
         _client = new DiscordSocketClient(_config);
+
+        // Create a new command service and command handler
+        _commandService = new(); // apparently new() is the new hotness? 
+        _commandHandler = new(_client, _commandService);
+
         _client.Log += Log;
 
         await StartBot(); 
@@ -35,19 +52,24 @@ public  class Program
 
     public async Task StartBot()
     {
-        Console.WriteLine("Checking for config.xml and announcement.xml...");
-        CheckForFiles();
-        Console.WriteLine("Done!");
-
         await _client.LoginAsync(TokenType.Bot, token);
         await _client.StartAsync();
 
+        // Load our modules/commands
+        await _commandHandler.InstallCommandsAsync();
+        _client.SlashCommandExecuted += SlashCommandHandler;
+        //_client.InteractionCreated += HandleInteractionAsync;
+
+
+        // Listen for when the client is ready
         _client.Ready += () =>
         {
             Console.WriteLine("Ready!");
             OnReady();
             return Task.CompletedTask;
         };
+
+        //_client.MessageReceived += _commandHandler.HandleCommandAsync;
 
         // Block this task until the program is closed
         // Uncomment to keep the task open after finishing
@@ -58,6 +80,7 @@ public  class Program
     {
         // WE ARE READY TO DO STUFF NOW
 
+        // Once the bot is beefier, we will delegate these to mod commands
         Console.WriteLine("Starting bulk delete...");
         await BulkDelete(250);
         Console.WriteLine("Bulk delete finished!");
@@ -65,6 +88,42 @@ public  class Program
         Console.WriteLine("Starting PostFromXML...");
         await PostFromXML();
         Console.WriteLine("PostFromXML done! You should be safe to close the window now.");
+
+        await SlashCommands();
+    }
+
+    private async Task SlashCommands()
+    {
+        // Set up slash commands
+        var serverCommand = new SlashCommandBuilder();
+        serverCommand.WithName("echo-server");
+        serverCommand.WithDescription("echoes a message");
+
+        // Let's do our global command
+        var globalCommand = new SlashCommandBuilder();
+        globalCommand.WithName("echo");
+        globalCommand.WithDescription("Echoes a message");
+
+        try
+        {
+            // With global commands we don't need the guild.
+            await _client.CreateGlobalApplicationCommandAsync(globalCommand.Build());
+            // Using the ready event is a simple implementation for the sake of the example. Suitable for testing and development.
+            // For a production bot, it is recommended to only run the CreateGlobalApplicationCommandAsync() once for each command.
+        }
+        catch (ApplicationCommandException exception)
+        {
+            // If our command was invalid, we should catch an ApplicationCommandException. This exception contains the path of the error as well as the error message. You can serialize the Error field in the exception to get a visual of where your error is.
+            var json = JsonConvert.SerializeObject(exception.Reason, Formatting.Indented);
+
+            // You can send this error somewhere or just print it to the console, for this example we're just going to print it.
+            Console.WriteLine(json);
+        }
+    }
+
+    private async Task SlashCommandHandler(SocketSlashCommand command)
+    {
+        await command.RespondAsync($"So this happened: {command.Data.Name}");
     }
 
     public void CheckForFiles()
@@ -206,4 +265,70 @@ public class ConfigFromXML
     public string BotToken { get; set; }
     public string GuildID { get; set; }
     public string TextChannelID { get; set; }
+}
+
+public class CommandHandler
+{
+    private readonly DiscordSocketClient _client;
+    private readonly CommandService _commands;
+
+    // Retrieve client and CommandService instance via ctor
+    public CommandHandler(DiscordSocketClient client, CommandService commands)
+    {
+        _commands = commands;
+        _client = client;
+    }
+
+    public async Task InstallCommandsAsync()
+    {
+        // Hook the MessageReceived event into our command handler
+        _client.MessageReceived += HandleCommandAsync;
+
+        // Here we discover all of the command modules in the entry 
+        // assembly and load them. Starting from Discord.NET 2.0, a
+        // service provider is required to be passed into the
+        // module registration method to inject the 
+        // required dependencies.
+        //
+        // If you do not use Dependency Injection, pass null.
+        // See Dependency Injection guide for more information.
+        await _commands.AddModulesAsync(assembly: Assembly.GetEntryAssembly(),
+                                        services: null);
+    }
+
+    private async Task HandleCommandAsync(SocketMessage messageParam)
+    {
+        // Don't process the command if it was a system message
+        var message = messageParam as SocketUserMessage;
+        if (message == null) return;
+
+        // Create a number to track where the prefix ends and the command begins
+        int argPos = 0;
+
+        // Determine if the message is a command based on the prefix and make sure no bots trigger commands
+        if (!(message.HasCharPrefix('!', ref argPos) ||
+            message.HasMentionPrefix(_client.CurrentUser, ref argPos)) ||
+            message.Author.IsBot)
+            return;
+
+        // Create a WebSocket-based command context based on the message
+        var context = new SocketCommandContext(_client, message);
+
+        // Execute the command with the command context we just
+        // created, along with the service provider for precondition checks.
+        await _commands.ExecuteAsync(
+            context: context,
+            argPos: argPos,
+            services: null);
+    }
+}
+
+// Create a module with no prefix
+public class MyModule : ModuleBase<SocketCommandContext>
+{
+    [SlashCommand("hello", "Says hello to the user.")]
+    public async Task HelloAsync()
+    {
+        await ReplyAsync("Hello, world!");
+    }
 }
